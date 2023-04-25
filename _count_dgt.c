44bd96010ccd04ae0dd0fc70b01941080aaab1f4
twiceTheProgrammer <njabulozweli08@gmail.com>
2023-04-25 16:19:35 -0700

    + count digit file

---
+++ _count_dgt.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * count_digit - returns cnt of fgt
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
+}
